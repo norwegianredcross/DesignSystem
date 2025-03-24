02be5d35b0ccec2ff9a778f2ab724c4eb46ebe04
MichaelAav <144216966+MichaelAav@users.noreply.github.com>
2025-03-24 12:14:12 +0100

    Replaced entire repository to the one working

--- design-tokens-build/colors.d.ts
+++ design-tokens-build/colors.d.ts
@@ -3,14 +3,7 @@ import type {} from '@digdir/designsystemet-react/colors';
 declare module '@digdir/designsystemet-react/colors' {
   export interface MainAndSupportColors {
     primary: never;
-    accent: never;
-    extra1: never;
-    extra2: never;
-    system1: never;
-    system2: never;
-    system3: never;
-    system4: never;
-    system5: never;
-    system6: never;
+    "secondary-hav": never;
+    "secondary-jungel": never;
   }
 }
