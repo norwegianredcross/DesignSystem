a28fe6ac787969f58472b8b704cd0a89b867aad6
MichaelAav <144216966+MichaelAav@users.noreply.github.com>
2025-03-24 13:15:37 +0100

    testing

--- design-tokens-build/colors.d.ts
+++ design-tokens-build/colors.d.ts
@@ -3,7 +3,7 @@ import type {} from '@digdir/designsystemet-react/colors';
 declare module '@digdir/designsystemet-react/colors' {
   export interface MainAndSupportColors {
     primary: never;
-    "secondary-hav": never;
-    "secondary-jungel": never;
+    secondary-hav: never;
+    secondary-jungel: never;
   }
 }
